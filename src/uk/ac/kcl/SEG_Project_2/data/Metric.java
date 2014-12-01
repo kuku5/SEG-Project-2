f8db4b8572d2c1e9fdf2c15d2c793e4b55b1d58e
Mark Ormesher <me@markormesher.co.uk>
2014-12-01 14:06:51 +0000

    Added link from metric screen to data screen

--- src/uk/ac/kcl/SEG_Project_2/data/Metric.java
+++ src/uk/ac/kcl/SEG_Project_2/data/Metric.java
@@ -1,6 +1,9 @@
 package uk.ac.kcl.SEG_Project_2.data;
 
-public class Metric {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Metric implements Parcelable{
 
 	private String name;
 	private String info;
@@ -10,6 +13,12 @@ public class Metric {
 		this.name = name;
 		this.info = info;
 		this.iconId = iconId;
+	}
+
+	public Metric(Parcel in) {
+		this.name = in.readString();
+		this.info = in.readString();
+		this.iconId = in.readInt();
 	}
 
 	public String getName() {
@@ -23,4 +32,26 @@ public class Metric {
 	public int getIconId() {
 		return iconId;
 	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(name);
+		dest.writeString(info);
+		dest.writeInt(iconId);
+	}
+
+	public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+		public Metric createFromParcel(Parcel in) {
+			return new Metric(in);
+		}
+
+		public Metric[] newArray(int size) {
+			return new Metric[size];
+		}
+	};
 }
